99849c5c2f60f22f6c5228f36160bf0c5bff492c
sahilw25 <walawalkarsahil25@gmail.com>
2024-06-17 23:03:32 +0530

    E-comm cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,13 +1,34 @@
-const { getProductById } = require("../models/Product");
-const { addProductToCart } = require("../models/cart");
+const { addProductToCart, getCartDetailsFromFile } = require('../models/cart');
+const { getProductById, fetchAllProducts } = require('../models/Product');
 
-exports.postCartPage = (req,res) => {
-    const productId = req.body.productId;
-    getProductById(productId, product => {
-        addProductToCart(productId, product.price );
-        res.redirect('/');
+exports.postCartPage = (req, res) => {
+  const productId = req.body.productId;
+  getProductById(productId, (product) => {
+    addProductToCart(productId, product.price);
+    res.redirect('/');
+  });
+};
+
+exports.getCartPage = (req, res) => {
+  getCartDetailsFromFile((cart) => {
+    const cartProducts = cart.products;
+    fetchAllProducts((products) => {
+      const productsData = [];
+      let totalPrice = 0;
+      for (let cartItem of cartProducts) {
+        let singleProduct = products.find((prod) => prod.id.toString() === cartItem.id.toString());
+        cartProductPrice = +cartItem.quantity * +singleProduct.price;
+        totalPrice += cartProductPrice;
+        productsData.push({ ...singleProduct, quantity: cartItem.quantity, cartPrice: cartProductPrice });
+      }
+
+      const viewsData = {
+        pageTitle: 'Cart Details',
+        cartProducts: productsData,
+        totalPrice
+      };
+
+      res.render('cartDetails', viewsData);
     });
-
-}
-
-exports.getCartPage = (req,res) => {}
+  });
+};
--- models/cart.js
+++ models/cart.js
@@ -2,50 +2,50 @@ const fs = require('fs');
 const rootDir = require('../util/path');
 const path = require('path');
 
-const getCartDetailFromFile = (callBack) => {
-    const cartPath = path.join(rootDir, 'data', 'cart.json');
-    fs.readFile(cartPath, (error, cartContent) => {
-        let cart = {products: []};
+exports.getCartDetailsFromFile = (callBack) => {
+  const cartPath = path.join(rootDir, 'data', 'cart.json');
+  fs.readFile(cartPath, (error, cartContent) => {
+    let cart = { products: [] };
+    //if cart data exists
+    if (!error) {
+      cart = JSON.parse(cartContent);
+    }
 
-        if(!error){
-            cart = JSON.parse(cartContent);
-        }
-
-        return callBack(cart);
-    });
-}
+    return callBack(cart);
+  });
+};
 
 exports.addProductToCart = (productId, productPrice) => {
-    const cartPath = path.join(rootDir, 'data', 'cart.json');
+  const cartPath = path.join(rootDir, 'data', 'cart.json');
 
-    getCartDetailFromFile(cart => {
-        let existingProductIndex = cart.products.findIndex((prod) => prod.id.toString() == productId);
-        let updatedProduct;
+  this.getCartDetailsFromFile((cart) => {
+    let existingProductIndex = cart.products.findIndex((prod) => prod.id.toString() === productId);
+    let updatedProduct;
 
-        if(existingProductIndex != -1){
-            updatedProduct = {...cart.products[existingProductIndex]};
-            updatedProduct.quantity += 1;
-            cart.products = [...cart.products];
-            cart.products[existingProductIndex] = updatedProduct;
-        } else{
-            updatedProduct = { id: productId, quantity:1};
-            cart.products = [...cart.products, updatedProduct];
-        }
+    if (existingProductIndex !== -1) {
+      updatedProduct = { ...cart.products[existingProductIndex] };
+      updatedProduct.quantity += 1;
+      cart.products = [...cart.products];
+      cart.products[existingProductIndex] = updatedProduct;
+    } else {
+      updatedProduct = { id: productId, quantity: 1 };
+      cart.products = [...cart.products, updatedProduct];
+    }
 
-        fs.writeFile(cartPath, JSON.stringify(cart), (error) => {
-            console.log(error);
-        });
+    fs.writeFile(cartPath, JSON.stringify(cart), (error) => {
+      console.log(error);
     });
+  });
 };
 
 exports.deleteProductFromCart = (productId) => {
-    const cartPath = path.join(rootDir, 'data', 'cart.json');
-    getCartDetailFromFile(cart => {
-        let cartProducts = cart.products;
-        let updatedCartProducts = cartProducts.filter(prod => prod.id.toString() != productId.toString());
+  const cartPath = path.join(rootDir, 'data', 'cart.json');
+  this.getCartDetailsFromFile((cart) => {
+    let cartProducts = cart.products;
+    let updatedCartProducts = cartProducts.filter((prod) => prod.id.toString() !== productId.toString());
 
-        fs.writeFile(cartPath, JSON.stringify(updatedCartProducts), (error) => {
-            console.log(error);
-        });
+    fs.writeFile(cartPath, JSON.stringify({ products: updatedCartProducts }), (error) => {
+      console.log(error);
     });
+  });
 };
